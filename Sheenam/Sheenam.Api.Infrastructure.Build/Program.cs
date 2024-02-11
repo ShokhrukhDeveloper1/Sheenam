eb83079f70e56d7d41dc24b7e67738195ea3a975
Shokhrukh <ShokhrukhDeveloper1@gmail.com>
2024-02-11 22:27:46 +0500

    INFRA: Initialize Build Project

---
+++ Sheenam/Sheenam.Api.Infrastructure.Build/Program.cs
@@ -0,0 +1,78 @@
+﻿//==================================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free To Use Comfort and Peace
+//==================================================
+
+using ADotNet.Clients;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets.Tasks;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets.Tasks.SetupDotNetTaskV1s;
+
+var githubPipeline = new GithubPipeline
+{
+    Name = "Sheenam Build Pipeline",
+
+    OnEvents = new Events
+    {
+        PullRequest = new PullRequestEvent
+        {
+            Branches = new string[] { "master" }
+        },
+
+        Push = new PushEvent
+        {
+            Branches = new string[] { "master" }
+        }
+    },
+
+    Jobs = new Dictionary<string, Job>
+    {
+        {
+            "build",
+            new Job
+            {
+                RunsOn = BuildMachines.Windows2022,
+
+                Steps = new List<GithubTask>
+                {
+                    new CheckoutTaskV2
+                    {
+                        Name = "Check out"
+                    },
+
+                    new SetupDotNetTaskV1
+                    {
+                        Name = "Setup .Net",
+
+                        TargetDotNetVersion = new TargetDotNetVersion
+                        {
+                            DotNetVersion = "6.0.418",
+                            IncludePrerelease = true
+                        }
+                    },
+
+                    new RestoreTask
+                    {
+                        Name = "Restore"
+                    },
+
+                    new DotNetBuildTask
+                    {
+                        Name = "Build"
+                    },
+
+                    new TestTask
+                    {
+                        Name = "Test"
+                    }
+                }
+            }
+        }
+    }
+};
+
+var client = new ADotNetClient();
+
+client.SerializeAndWriteToFile(
+    adoPipeline: githubPipeline,
+    path: "../../../../../.github/workflows/dotnet.yml");
