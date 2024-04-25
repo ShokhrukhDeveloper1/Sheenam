6d6f1fc1f228fbe338c249061aa472436f0f4ff7
Shokhrukh <ShokhrukhDeveloper1@gmail.com>
2024-04-25 23:14:00 +0500

    CONFIG: Register Broker

--- Sheenam/Sheenam.Api/Startup.cs
+++ Sheenam/Sheenam.Api/Startup.cs
@@ -30,6 +30,7 @@ namespace Sheenam.Api
 
             services.AddDbContext<StorageBroker>();
             services.AddControllers();
+            services.AddTransient<IStorageBroker, StorageBroker>();
 
             services.AddSwaggerGen(option =>
             {
